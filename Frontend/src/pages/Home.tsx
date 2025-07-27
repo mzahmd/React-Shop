60308018c112f87b7cad5af17644f46814526678
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-07-27 18:17:30 +0200

    some animation for product cards

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { MotionEffect } from '@/components/animate-ui/effects/motion-effect';
 import Header from "@/components/Header";
 import ProductCard from "@/components/ProductCard";
 import { useGetProducts } from "@/hooks/useGetProducts";
@@ -9,13 +10,22 @@ export default function Home() {
     <>
       <Header />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mb-5">
-        {products.map(product => (
-          <ProductCard
+        {products.map((product, index) => (
+          <MotionEffect
             key={product.id}
-            title={product.title}
-            image={product.image}
-            price={product.price}
-          />
+            slide={{
+              direction: 'down',
+            }}
+            fade
+            zoom
+            delay={0.5 + index * 0.1}
+          >
+            <ProductCard
+              title={product.title}
+              image={product.image}
+              price={product.price}
+            />
+          </MotionEffect>
         ))}
       </div>
     </>
