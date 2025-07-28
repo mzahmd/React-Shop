df3f1cef8973d27eab57a6f21a78bc8e60823f97
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-07-28 12:49:26 +0200

    storing hashed password

--- Backend/src/main/java/com/example/backend/Authentication/AuthenticationService.java
+++ Backend/src/main/java/com/example/backend/Authentication/AuthenticationService.java
@@ -2,6 +2,7 @@ package com.example.backend.Authentication;
 
 import com.example.backend.Customer.Customer;
 import com.example.backend.Customer.CustomerDAO;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -9,9 +10,11 @@ import java.util.Optional;
 @Service
 public class AuthenticationService {
     private final CustomerDAO customerDAO;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthenticationService(CustomerDAO customerDAO) {
+    public AuthenticationService(CustomerDAO customerDAO, PasswordEncoder passwordEncoder) {
         this.customerDAO = customerDAO;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void register(Customer c) {
@@ -19,17 +22,17 @@ public class AuthenticationService {
             throw new IllegalStateException("Customer already exists!");
         }
 
-        customerDAO.registerCustomer(c);
+        customerDAO.registerCustomer(new Customer(c.getEmail(), passwordEncoder.encode(c.getPassword())));
     }
 
-    public void login(Customer c) {
-        Optional<Customer> customer = customerDAO.findCustomerByEmail(c);
+    public void login(Customer logCustomer) {
+        Optional<Customer> customer = customerDAO.findCustomerByEmail(logCustomer);
 
         if (customer.isEmpty()) {
             throw new IllegalStateException("Customer doesn't exists!");
         }
 
-        if(!customer.get().getPassword().equals(c.getPassword())) {
+        if (!passwordEncoder.matches(logCustomer.getPassword(), customer.get().getPassword())) {
             throw new IllegalStateException("Bad credentials!");
         }
     }
