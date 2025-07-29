653da87fa6561dff5450e078acb8a179a7741183
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-07-29 11:26:34 +0200

    Add datatable for users

--- Frontend/src/pages/Users.tsx
+++ Frontend/src/pages/Users.tsx
@@ -1,16 +1,73 @@
+import { type ColumnDef, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { useGetUsers } from "@/hooks/useGetUsers";
+import type { User } from "@/interface/User";
+
+
+const columns: ColumnDef<User>[] = [
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "role",
+    header: "Role",
+  },
+]
 
 export default function Users() {
   const users = useGetUsers();
+  const table = useReactTable({
+    data: users,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
 
   return (
-    <>
-      {users.map((user) => (
-        <div key={user.email} className="p-4 border-b">
-          <h2 className="text-lg font-semibold">{user.email}</h2>
-          <p className="text-sm text-gray-600">{user.email}</p>
-        </div>
-      ))}
-    </>
+    <div className="overflow-hidden rounded-md border w-11/12 mx-auto mt-10">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                  </TableHead>
+                )
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
   );
 }
