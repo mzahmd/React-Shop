7de2acb72fa9afeac99f93b4d22626d60e0ef85a
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-08-02 17:05:30 +0200

    logout

--- Frontend/src/hooks/useLogout.ts
+++ Frontend/src/hooks/useLogout.ts
@@ -1,9 +1,12 @@
 import { Router } from "@/router"
+import { apiClient } from "@/services/api-client"
 
 export default function useLogout() {
   function logout() {
-    localStorage.removeItem("user")
-    Router.push("AuthLogin")
+    apiClient.post("/auth/logout").then(() => {
+      localStorage.removeItem("user")
+      Router.push("AuthLogin")
+    })
   }
 
   return { logout }
