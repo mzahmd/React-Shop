2062e9b291481b86bbb3885eb5b20fbbfd1e993b
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-08-05 08:44:51 +0200

    removed unused code from Ensure Auth

--- Frontend/src/components/EnsureAuth.tsx
+++ Frontend/src/components/EnsureAuth.tsx
@@ -1,31 +1,13 @@
 import { useEffect } from "react"
-import { toast } from "sonner"
 
 import { useUserAuthenticated } from "@/hooks/useUserAuthenticated"
 import { AuthRouter } from "@/pages/AuthArea/router"
-import { apiClient } from "@/services/api-client"
 
 interface EnsureAuthProps {
   children: React.ReactNode
 }
 
 export default function EnsureAuth({ children }: EnsureAuthProps) {
-  // useEffect(() => {
-  //   async function checkAuth() {
-  //     await apiClient.get("/user/authenticated")
-  //       .then(response => {
-  //         if (!response.data) {
-  //           AuthRouter.push("Login")
-  //         }
-  //       }).catch(() => {
-  //         toast("An Error has happend")
-  //         AuthRouter.push("Login")
-  //       })
-  //   }
-
-  //   checkAuth()
-  // }, [])
-
   const isAuthenticated = useUserAuthenticated()
 
   useEffect(() => {
