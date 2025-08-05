40d56e32f7c05d249fd20f9570d23ceaf604e8f5
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-08-05 15:45:02 +0200

    use cors config from yml

--- Backend/src/main/java/com/example/backend/WebConfig/WebConfig.java
+++ Backend/src/main/java/com/example/backend/WebConfig/WebConfig.java
@@ -1,19 +1,30 @@
 package com.example.backend.WebConfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("#{'${cors.allowed-origins}'}")
+    private String allowedOrigins;
+
+    @Value("#{'${cors.allowed-methods}'}")
+    private String[] allowedMethods;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        System.out.println(Arrays.toString(allowedMethods));
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods(allowedMethods)
                 .allowCredentials(true);
     }
 }
