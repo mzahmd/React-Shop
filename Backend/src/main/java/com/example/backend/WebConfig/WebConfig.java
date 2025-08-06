a96a1626fe658342f2f76a405d8f14926b137f06
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-08-06 17:34:17 +0200

    removed println

--- Backend/src/main/java/com/example/backend/WebConfig/WebConfig.java
+++ Backend/src/main/java/com/example/backend/WebConfig/WebConfig.java
@@ -6,9 +6,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
-import java.util.List;
-
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
@@ -21,7 +18,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        System.out.println(Arrays.toString(allowedMethods));
         registry.addMapping("/**")
                 .allowedOrigins(allowedOrigins)
                 .allowedMethods(allowedMethods)
