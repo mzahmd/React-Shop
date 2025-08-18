3eac6922472a6b9b51e6b171583611292de4afd6
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-08-18 13:59:52 +0200

    redirect if user is not Admin and prevent backend call

--- Frontend/src/components/EnsureIsAdmin.tsx
+++ Frontend/src/components/EnsureIsAdmin.tsx
@@ -6,7 +6,7 @@ import { HomeRouter } from "@/pages/HomeArea/router"
 interface EnsureIsAdminProps {
   children: React.ReactNode
 }
-
+// http://localhost:5173/admin/user
 export default function EnsureIsAdmin({ children }: EnsureIsAdminProps) {
   const { user } = useUserContext()
 
@@ -15,6 +15,14 @@ export default function EnsureIsAdmin({ children }: EnsureIsAdminProps) {
       HomeRouter.push("Home")
     }
   }, [user])
+
+  if (!user) {
+    return <>Loading...</>
+  }
+
+  if (user.role !== "ADMIN") {
+    return <>Redirecting...</>
+  }
 
   return (
     <>
