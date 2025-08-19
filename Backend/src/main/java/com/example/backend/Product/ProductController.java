889a5017567a7a1fbdb3ec0b071dea73605d55e9
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-08-19 20:41:25 +0200

    filtering products on the backend

--- Backend/src/main/java/com/example/backend/Product/ProductController.java
+++ Backend/src/main/java/com/example/backend/Product/ProductController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -17,16 +19,51 @@ public class ProductController {
     @Value("#{'${product.url}'}")
     private String PRODUCT_URL;
 
+//    @GetMapping
+//    public Mono<List<Product>> getProducts() {
+//        WebClient.Builder builder = WebClient.builder();
+//
+//        return builder.build()
+//                .get()
+//                .uri(PRODUCT_URL)
+//                .accept(MediaType.APPLICATION_JSON)
+//                .retrieve()
+//                .bodyToFlux(Product.class)
+//                .collectList();
+//    }
+//
+//    @GetMapping
+//    public Mono<List<Product>> getProducts(@RequestParam(name = "category", defaultValue = "", required = false) String category) {
+//        WebClient.Builder builder = WebClient.builder();
+//
+//        return builder.build()
+//                .get()
+//                .uri(PRODUCT_URL)
+//                .accept(MediaType.APPLICATION_JSON)
+//                .retrieve()
+//                .bodyToFlux(Product.class)
+//                .filterWhen(product -> Mono.just(product.getCategory().equals(category)))
+//                .collectList();
+//    }
+
     @GetMapping
-    public Mono<List<Product>> getProducts() {
+    public Mono<List<Product>> getProducts(@RequestParam(name = "category", defaultValue = "", required = false) String category) {
         WebClient.Builder builder = WebClient.builder();
 
-        return builder.build()
+        Flux<Product> productFlux = builder.build()
                 .get()
                 .uri(PRODUCT_URL)
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
-                .bodyToFlux(Product.class)
-                .collectList();
+                .bodyToFlux(Product.class);
+
+        if (!category.isEmpty()) {
+            return productFlux.filter(product -> product.getCategory().equals(category))
+                    .collectList();
+        }
+
+        return productFlux.collectList();
     }
+
 }
+
