537e858418a0574caf28724db031d7827b705f90
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-08-22 16:20:36 +0200

    small adjustment for the JUnit Tests

--- Backend/src/test/java/com/example/backend/User/UserListDataAccessServiceTest.java
+++ Backend/src/test/java/com/example/backend/User/UserListDataAccessServiceTest.java
@@ -3,15 +3,12 @@ package com.example.backend.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class UserListDataAccessServiceTest {
-
-    // MockitoAnnotations.initMocks(this);
 
     UserListDataAccessService underTest;
 
