11171e13c92a9dd57fe321acbf326e1640d7e41f
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-08-25 15:13:47 +0200

    created Order class for mapping Products and Users

---
+++ Backend/src/main/java/com/example/backend/Order/Order.java
@@ -0,0 +1,22 @@
+package com.example.backend.Order;
+
+public class Order {
+    int userID;
+    int productID;
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+}
