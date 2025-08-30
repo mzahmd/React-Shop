255e32959f75b1910c9db3d54e0d7831d454b0a3
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-08-30 22:45:37 +0200

    remove DTO Interface not used

--- Frontend/src/interface/IProduct.ts
+++ Frontend/src/interface/IProduct.ts
@@ -1,11 +1,8 @@
-export interface IProductDTO {
-  title: string
-  price: number
-  image: string
-}
-
-export interface IProduct extends IProductDTO {
+export interface IProduct {
   id: number
   description: string
   category: string
+  title: string
+  price: number
+  image: string
 }
