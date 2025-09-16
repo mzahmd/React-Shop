cd8cb850f9d980c63dce81f9ed7f1b5767a0597f
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-09-16 18:16:17 +0200

    refactor @RequestParam and add @RequestMapping for /order

--- Backend/src/main/java/com/example/backend/controller/OrderController.java
+++ Backend/src/main/java/com/example/backend/controller/OrderController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/order")
 public class OrderController {
     private final OrderService orderService;
 
@@ -18,12 +20,12 @@ public class OrderController {
         this.orderService = orderService;
     }
 
-    @GetMapping("/order")
+    @GetMapping
     public List<Order> getOrdersFromUser() {
         return orderService.getOrdersFromUser();
     }
 
-    @PostMapping("/order")
+    @PostMapping
     public void createOrder(@RequestBody List<OrderRequestDTO> orderRequestDTO) {
         orderService.createOrder(orderRequestDTO);
     }
